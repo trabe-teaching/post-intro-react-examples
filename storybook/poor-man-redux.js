9ef4bcc3f5e1709277ea1c5bb410567b3868bb6f
David Barral <david@trabe.io>
2017-06-16 14:44:50 +0200

    Alternative poor man redux example

--- storybook/poor-man-redux.js
+++ storybook/poor-man-redux.js
@@ -1,39 +1,79 @@
 import React, { Component, Children } from "react";
-import * as PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import { storiesOf } from "@kadira/storybook";
 
+const createStore = (reducer, initialState) => {
+  let state = reducer(initialState, { type: "@@INIT@@" });
+  let listeners = [];
 
-const createStore = reducer => {
+  const getState = () => state;
 
-  let state = reducer(undefined, { type: "@@INIT@@" });
-  let subscribers = [];
+  const subscribe = listener => {
+    listeners = [...listeners, listener];
+    return () => {
+      listeners = listeners.select(l => l !== listener);
+    };
+  };
 
-  const notify = () => subscribers.forEach(s => s(state));
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(l => l());
+  };
 
   return {
-    getState: () => {
-      return state;
-    },
-
-    subscribe: (subscriber) => {
-      subscribers = [...subscribers, subscriber];
-      return () => {
-        subscribers = subscribers.filter(s => s !== subscriber);
-      }
-    },
-
-    dispatch(action) {
-      state = reducer(state, action);
-      notify();
-    },
+    getState,
+    subscribe,
+    dispatch,
   };
 };
 
+const initialState = {
+  count: 0,
+  potato: "nope",
+};
+
+const INCREMENT = "INCREMENT";
+const PATATIZE = "PATATIZE";
+const increment = () => ({ type: INCREMENT });
+
+const potato = (state = initialState.potato, { type }) => {
+  switch(type) {
+    case PATATIZE:
+      return "yes";
+
+    default:
+      return state;
+  }
+};
+
+const count = (state = initialState.count, { type }) => {
+  switch(type) {
+    case INCREMENT:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+
+const combineReducers = reducers => (state, action) =>
+  Object.keys(reducers).reduce((s, k) => {
+    s[k] = reducers[k](state && state[k], action);
+    return s;
+  }, {});
+
+const getCount = state => state.count;
+
+
+
+const reducer = combineReducers({ count, potato });
+const store = createStore(reducer);
 
 class Provider extends Component {
+
   getChildContext() {
     return {
-      store: this.props.store
+      store: this.props.store,
     }
   }
 
@@ -47,65 +87,81 @@ Provider.childContextTypes = {
 };
 
 
-const initialState = {
-  count: 0,
-}
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return {
-        count: state.count + 1,
-      };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-
-const connect = (stateToProps = () =>({}), dispatchToProps = () => ({})) => WrappedComponent => {
-
+const connect = (mapStateToProps = () => ({}), mapDispatchToProps = () => ({})) => WrappedComponent => {
   class Wrapper extends Component {
 
-    constructor (props, context) {
-      super(props, context);
-      this.state = stateToProps(context.store.getState());
-      context.store.subscribe(newState => this.setState(newState));
+    state = {
+      reduxState: this.context.store.getState(),
+    }
+
+    componentDidMount() {
+      this.unsubscribe = this.context.store.subscribe(() => {
+        this.setState({
+          reduxState: this.context.store.getState(),
+        });
+      });
+    }
+
+    componentWillUnmount() {
+      if (this.unsubscribe) {
+        this.unsubscribe();
+      }
+    }
+
+    shouldComponentUpdate(nextState, nextProps) {
+      return this.state !== nextState;
     }
 
     render() {
-      return <WrappedComponent {...this.props} {...stateToProps(this.state)} {...dispatchToProps(this.context.store.dispatch)} />;
+      const computedProps = {
+        ...this.props,
+        ...mapStateToProps(this.state.reduxState, this.props),
+        ...mapDispatchToProps(this.context.store.dispatch, this.props),
+        dispatch: this.context.store.dispatch,
+      };
+
+      return <WrappedComponent {...computedProps } />
     }
   }
 
   Wrapper.contextTypes = Provider.childContextTypes;
 
+  Wrapper.displayName = `connected(${WrappedComponent.displayName})`;
   return Wrapper;
-}
+};
 
-const Counter = ({ count }) => <span>{count}</span>;
+
+const Counter = ({ count }) => <div>{count} clicks</div>;
 
 const ConnectedCounter = connect(
   state => ({
-    count: state.count
+    count: getCount(state),
   })
 )(Counter);
 
-const Button = ({ onClick }) => <button onClick={onClick}>+1</button>;
+const Button = ({ children, onClick }) => {
+  const handler = e => {
+    e.preventDefault();
+    onClick();
+  };
+
+  return <button onClick={handler}>{children}</button>;
+}
+
 
 const ConnectedButton = connect(
   undefined,
   dispatch => ({
-    onClick: () => dispatch({ type: "INCREMENT" }),
+    onClick: () => dispatch(increment()),
   })
 )(Button);
+
 
 const App = () => (
   <Provider store={store}>
     <div>
       <ConnectedCounter />
-      <ConnectedButton />
+      <ConnectedButton>Click me!</ConnectedButton>
     </div>
   </Provider>
 );
